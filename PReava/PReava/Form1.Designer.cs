dfb5cb9fb39adbfd8e967c6941cd070a177a3a1d
Guilherme Nobrega <guilherme.nobreg@hotmail.com>
2024-06-18 10:11:51 -0300

    Add files via upload

---
+++ PReava/PReava/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PReava
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.listBoxMostrar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(72, 112);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(197, 87);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(72, 257);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(197, 87);
+            this.btnLimpar.TabIndex = 1;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // listBoxMostrar
+            // 
+            this.listBoxMostrar.FormattingEnabled = true;
+            this.listBoxMostrar.Location = new System.Drawing.Point(335, 62);
+            this.listBoxMostrar.Name = "listBoxMostrar";
+            this.listBoxMostrar.Size = new System.Drawing.Size(435, 355);
+            this.listBoxMostrar.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxMostrar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnExecutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.ListBox listBoxMostrar;
+    }
+}
+
---
+++ PReava/PReava/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PReava
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+
+            double[] matrizA = new double[14];
+            double[] matrizB = new double[14];
+            string auxiliar;
+
+            for (int i = 1; i < 14; i++)
+            {
+                double j = i;
+                matrizA[i] = j;
+
+
+                if (matrizA[i] % 2 == 0)
+                {
+                    matrizB[i] = j * 4;
+                }
+                else
+                {
+                    matrizB[i] = j + 4;
+                }
+
+                auxiliar = ("Posição:" + i + " Matriz A = " + matrizA[(int)j] + " Matriz B =" + matrizB[(int)j]);
+                listBoxMostrar.Items.Add(auxiliar);
+            }
+            
+           
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            listBoxMostrar.Items.Clear();
+        }
+    }
+}
